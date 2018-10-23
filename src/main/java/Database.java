d9e44d7f584f46c50279aee80a7023c3dbab3516
harshakota <harshakota.hk@gmail.com>
2018-10-22 22:02:07 -0400

    Added Sensors Table

--- src/main/java/Database.java
+++ src/main/java/Database.java
@@ -19,6 +19,12 @@ public class Database {
         if (TableCreationChecks.getBatteryPercentage(hal.getPowerSources())) {
             createPowerTable();
         }
+
+
+        int noOfFans = TableCreationChecks.getFans(hal.getSensors());
+        if (noOfFans >= 1) {
+            createSensorsTable(noOfFans);
+        }
     }
 
     // Establish connection to the sqlite database/ Create if its doesn't exist
@@ -51,5 +57,30 @@ public class Database {
         }
     }
 
+    // Create a new Sensors table
+    private void createSensorsTable(int noOfFans){
+
+        StringBuilder fanColumnStatement = new StringBuilder();
+        String fanStatement0 = "FAN";
+        String fanStatement1 = " REAL NOT NULL";
+
+        for (int i=1; i<noOfFans; i++) {
+            fanColumnStatement.append(fanStatement0).append(i).append(fanStatement1).append(",");
+        }
+        fanColumnStatement.append(fanStatement0).append(noOfFans).append(fanStatement1);
+
+        try {
+            Statement senorsTableStatement = connection.createStatement();
+            String sql =
+                    "CREATE TABLE IF NOT EXISTS SENSORS " +
+                            "(TIMESTAMP INTEGER PRIMARY KEY   NOT NULL," + fanColumnStatement +")";
+            senorsTableStatement.executeUpdate(sql);
+            senorsTableStatement.close();
+            connection.close();
+        } catch (Exception e) {
+            log.error("Failed to create Sensor Table " + e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
+    }
 
 }
