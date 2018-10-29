59dcff8adb50340461a51033fe8832d115e9acde
Chris Byrnes <Christopher.Byrnes@marist.edu>
2018-10-29 16:32:38 -0400

    Added test classes

---
+++ src/tests/TableCreationChecksTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+import oshi.hardware.PowerSource;
+
+import static org.junit.Assert.*;
+
+public class TableCreationChecksTest {
+
+    @Test
+    public void getBatteryPercentage() {
+    }
+
+    @Test
+    public void getFans() {
+    }
+}
---
+++ src/tests/UITest.java
@@ -0,0 +1,18 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UITest {
+
+    @Test
+    public void start() {
+    }
+
+    @Test
+    public void run() {
+    }
+
+    @Test
+    public void stop() {
+    }
+}
