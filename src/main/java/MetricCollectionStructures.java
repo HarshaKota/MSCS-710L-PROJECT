56ca2a960a1c3500a4391cd44dd4104612ed7919
harshakota <harshakota.hk@gmail.com>
2018-11-04 16:28:24 -0500

    Added cpuStructure to hold cpu info when collection by the metric collector

--- src/main/java/MetricCollectionStructures.java
+++ src/main/java/MetricCollectionStructures.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class MetricCollectionStructures {
 
     public static class powerStructure {
@@ -31,5 +33,59 @@ public class MetricCollectionStructures {
         public long getTimestamp() {
             return timestamp;
         }
+    }
+
+    public static class cpuStructure {
+        long timestamp;
+        long uptime;
+        float userLoad;
+        float systemLoad;
+        float idelLoad;
+        ArrayList<Float> processorLoad = new ArrayList<>();
+
+        public long getTimestamp() {
+            return timestamp;
+        }
+
+        public void setTimestamp(long timestamp) {
+            this.timestamp = timestamp;
+        }
+
+        public long getUptime() {
+            return uptime;
+        }
+
+        public void setUptime(long uptime) {
+            this.uptime = uptime;
+        }
+
+        public float getUserLoad() {
+            return userLoad;
+        }
+
+        public void setUserLoad(float userLoad) {
+            this.userLoad = userLoad;
+        }
+
+        public float getSystemLoad() {
+            return systemLoad;
+        }
+
+        public void setSystemLoad(float systemLoad) {
+            this.systemLoad = systemLoad;
+        }
+
+        public float getIdelLoad() {
+            return idelLoad;
+        }
+
+        public void setIdelLoad(float idelLoad) {
+            this.idelLoad = idelLoad;
+        }
+
+        public void setProcessorLoad(Float processorLoad) {
+            this.processorLoad.add(processorLoad);
+        }
+
     }
 }
