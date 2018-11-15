4959db170f6493d8b27bd6c2083d3a954cc29169
BradleyLamitie <Bradley.Lamitie1@marist.edu>
2018-11-15 02:48:16 -0500

    Added javaFX Info to HomePage

--- src/main/java/UI.java
+++ src/main/java/UI.java
@@ -20,30 +20,38 @@ public class UI extends Application implements Runnable {
     @Override
     public void start(Stage stage) {
         Main.applicationOpen.set(true);
+        String javaVersion = System.getProperty("java.version");
+        String javafxVersion = System.getProperty("javafx.version");
 
         stage.setTitle("MetricsCollector Home");
+
         Label descriptionLabel = new Label("Select one of the following metrics and click the Submit button");
+        Label javaFXInfo = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".\n" +
+                "This is the value of the Application Status " + Main.applicationOpen);
+
         String availableMetrics[] = {"Power"};
+
         ComboBox dropdown = new ComboBox(FXCollections.observableArrayList(availableMetrics));
+
         dropdown.resize(600, 25);
+
         StackPane rootPane = new StackPane();
-        dropdown.setTranslateX(0);
 
         Pane dropDownPane = new Pane(dropdown);
-        Pane labelPane = new Pane(descriptionLabel);
-        labelPane.setTranslateX(50);
-        labelPane.setTranslateY(250);
         dropDownPane.setTranslateY(300);
         dropDownPane.setTranslateX(50);
 
-        rootPane.getChildren().addAll(labelPane, dropDownPane);
+        Pane labelPane = new Pane(descriptionLabel);
+        labelPane.setTranslateX(50);
+        labelPane.setTranslateY(250);
+
+        Pane javaFXInfoPane = new Pane(javaFXInfo);
+        javaFXInfoPane.setTranslateX(50);
+        javaFXInfoPane.setTranslateY(500);
+
+        rootPane.getChildren().addAll(javaFXInfoPane, labelPane, dropDownPane);
         Scene homeMenuScene = new Scene(rootPane, 800, 600);
 
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        Label javaFXInfo = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".\n" +
-                "This is the value of the Application Status " + Main.applicationOpen);
-//        Scene scene = new Scene(javaFXInfo, 640, 480);
         stage.setScene(homeMenuScene);
         stage.show();
     }
