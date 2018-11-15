aa9e4d54c0453abad39a648972d5255d6eb7a491
BradleyLamitie <Bradley.Lamitie1@marist.edu>
2018-11-15 02:40:54 -0500

    Added a drop down menu to UI

--- src/main/java/UI.java
+++ src/main/java/UI.java
@@ -1,6 +1,13 @@
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.TilePane;
 import javafx.stage.Stage;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -13,12 +20,24 @@ public class UI extends Application implements Runnable {
     @Override
     public void start(Stage stage) {
         Main.applicationOpen.set(true);
+
+        stage.setTitle("MetricsCollector Home");
+        Label descriptionLabel = new Label("Select one of the following metrics and click the Submit button");
+        String availableMetrics[] = {"Power"};
+        ComboBox dropdown = new ComboBox(FXCollections.observableArrayList(availableMetrics));
+        StackPane rootPane = new StackPane();
+        Pane dropDownPane = new Pane(dropdown);
+        Pane labelPane = new Pane(descriptionLabel);
+        rootPane.getChildren().addAll(dropDownPane);
+//        TilePane pane = new TilePane(dropdown);
+        Scene homeMenuScene = new Scene(rootPane, 800, 600);
+
         String javaVersion = System.getProperty("java.version");
         String javafxVersion = System.getProperty("javafx.version");
-        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".\n" +
+        Label javaFXInfo = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".\n" +
                 "This is the value of the Application Status " + Main.applicationOpen);
-        Scene scene = new Scene(l, 640, 480);
-        stage.setScene(scene);
+//        Scene scene = new Scene(javaFXInfo, 640, 480);
+        stage.setScene(homeMenuScene);
         stage.show();
     }
 
