839ee9fc83b81410a751a41d9c13617814dc885b
BradleyLamitie <Bradley.Lamitie1@marist.edu>
2018-11-15 10:23:12 -0500

    Added javaFX button to HomePage

--- src/main/java/UI.java
+++ src/main/java/UI.java
@@ -2,12 +2,14 @@ import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.TilePane;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -25,7 +27,7 @@ public class UI extends Application implements Runnable {
 
         stage.setTitle("MetricsCollector Home");
 
-        Label descriptionLabel = new Label("Select one of the following metrics and click the Submit button");
+        Label descriptionLabel = new Label("Select one of the following metrics from the drop-down box and click the Submit button:");
         Label javaFXInfo = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".\n" +
                 "This is the value of the Application Status " + Main.applicationOpen);
 
@@ -33,14 +35,23 @@ public class UI extends Application implements Runnable {
 
         ComboBox dropdown = new ComboBox(FXCollections.observableArrayList(availableMetrics));
 
+        Button button = new Button("Submit");
+        button.resize(500, 50);
+
         dropdown.resize(600, 25);
 
         StackPane rootPane = new StackPane();
 
+        Pane buttonPane = new Pane(button);
+        buttonPane.setTranslateX(675);
+        buttonPane.setTranslateY(300);
+        buttonPane.setTranslateZ(5);
+
         Pane dropDownPane = new Pane(dropdown);
+        final Rectangle outputClip = new Rectangle(600,50);
         dropDownPane.setTranslateY(300);
         dropDownPane.setTranslateX(50);
-
+        dropDownPane.setClip(outputClip);
         Pane labelPane = new Pane(descriptionLabel);
         labelPane.setTranslateX(50);
         labelPane.setTranslateY(250);
@@ -49,7 +60,7 @@ public class UI extends Application implements Runnable {
         javaFXInfoPane.setTranslateX(50);
         javaFXInfoPane.setTranslateY(500);
 
-        rootPane.getChildren().addAll(javaFXInfoPane, labelPane, dropDownPane);
+        rootPane.getChildren().addAll(javaFXInfoPane, labelPane, buttonPane, dropDownPane);
         Scene homeMenuScene = new Scene(rootPane, 800, 600);
 
         stage.setScene(homeMenuScene);
