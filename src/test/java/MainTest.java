235f3c4b026a83bea09d9155891650387ead16ad
BradleyLamitie <Bradley.Lamitie1@marist.edu>
2018-11-28 02:47:50 -0500

    Added test case for when conversion from date to long succeeds

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,4 +1,10 @@
+import org.junit.Assert;
 import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.text.ParseException;
+
+import static org.mockito.BDDMockito.given;
 
 public class MainTest {
 
@@ -7,5 +13,13 @@ public class MainTest {
         Main.databaseUrl = null;
         Main.main(null);
     }
+
+    @Test
+    public void succeededToParseDate() {
+        final Util util = Mockito.spy(new Util());
+        long testTime = util.convertDateToLong("11/28/2018-02:32:02");
+        Assert.assertEquals(testTime, 1543390322000L);
+    }
+
 
 }
