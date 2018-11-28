bc98980d23aed964f75dda2c95bc16b54cd7889f
BradleyLamitie <Bradley.Lamitie1@marist.edu>
2018-11-28 05:29:20 -0500

    Added a test case to test when the db fails to get Memory Metrics

--- src/test/java/DatabaseTest.java
+++ src/test/java/DatabaseTest.java
@@ -53,6 +53,14 @@ public class DatabaseTest {
         }
     }
 
+ 
+
+    @Test(expected = Exception.class)
+    public void failedToGetMemoryMetrics() throws Exception {
+        final Database dbObj = Mockito.spy(new Database());
+        given(dbObj.getMemoryMetrics(1L,1L,"")).willThrow(new Exception());
+    }
+
     @Test(expected = Exception.class)
     public void nullTimesInNetworkMetrics() throws Exception {
         final Database dbObj = Mockito.spy(new Database());
